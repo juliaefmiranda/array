2cbb206924b9bbfa8945be3e52ba5c62b19c1ba4
Julia <jerlo180908@gmail.com>
2025-04-25 11:07:33 -0300

    exercício 9 - array

---
+++ array9.js
@@ -0,0 +1,7 @@
+let frutas = ['Melancia', 'Uva', 'Morango','Banana', 'Abacaxi', 'Lichia'];
+
+let quantidade = frutas.length;
+
+console.log(`No total há ${quantidade} frutas.`)
+
+//Julia Miranda, 25/04
