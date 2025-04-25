f19f9ff0c6895f2b5c171b2bd44826e137bacb23
Julia <jerlo180908@gmail.com>
2025-04-25 10:55:35 -0300

    exercício 8 - array

---
+++ array8.js
@@ -0,0 +1,12 @@
+let idade = ['12', '20', '15', '14', '23','18'];
+let idadeM = [];
+
+for(let i = 0; i < idade.length; i++){
+    if(idade[i] >= 18){
+        idadeM.push(idade[i]);
+    }
+}
+
+console.log(idadeM);
+
+//Julia Miranda, 25/04
