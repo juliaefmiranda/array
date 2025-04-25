b94a0d73576d90889e031634b54639ea88899655
Julia <jerlo180908@gmail.com>
2025-04-25 09:37:28 -0300

    exercício 2 - array

---
+++ array2.js
@@ -0,0 +1,8 @@
+let musicas = ['Poesia Acústica - 13', 'A Morte do Autotune', 'Me encontra', 'Amor de Fim de Noite'];
+let quantidade = musicas.length;
+
+for(let i = 0; i < quantidade; i++){
+    console.log(musicas[i]);
+}
+
+//Juliam Miranda, 25/04
